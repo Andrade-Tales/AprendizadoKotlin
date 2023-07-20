27243674c63d0c6fbef59d4d50e4bddbe157604c
Andrade-Tales <selatedardna.1476@gmail.com>
2023-07-20 15:57:39 -0300

    Criada Lista de livros

---
+++ app/src/main/java/com/app/aprendizadokotlin/colecoesarrayslistas/teste/lists/Livro.kt
@@ -0,0 +1,10 @@
+package com.app.aprendizadokotlin.colecoesarrayslistas.teste.lists
+
+data class Livro (
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+        )
+
+
