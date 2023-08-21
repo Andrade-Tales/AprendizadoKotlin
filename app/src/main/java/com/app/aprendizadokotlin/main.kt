473659f12764493af4cf56bce61ec7e1ce75e7a5
Andrade-Tales <selatedardna.1476@gmail.com>
2023-08-21 16:54:33 -0300

    Implementado scope function, with e run

--- app/src/main/java/com/app/aprendizadokotlin/main.kt
+++ app/src/main/java/com/app/aprendizadokotlin/main.kt
@@ -1,11 +1,12 @@
 package com.app.aprendizadokotlin.colecoesarrayslistas
 
+import com.app.aprendizadokotlin.paradigmafuncional.testaImplementacaoComLet
 import com.app.aprendizadokotlin.paradigmafuncional.testaTipoFuncaoAnonima
 import com.app.aprendizadokotlin.paradigmafuncional.testaTipoFuncaoLambda
 
 
 fun main() {
-  testaTipoFuncaoAnonima()
+  testaImplementacaoComLet()
 
 }
 
---
+++ app/src/main/java/com/app/aprendizadokotlin/paradigmafuncional/testaScopeFunction.kt
@@ -0,0 +1,4 @@
+package com.app.aprendizadokotlin.paradigmafuncional
+
+class testaScopeFunction {
+}
